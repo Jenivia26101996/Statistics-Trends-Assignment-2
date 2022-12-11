3dd429bb88a297a553a349ba2310ac2284ec90c5
Jenivia26101996 <jenivia26@gmail.com>
2022-12-11 15:09:57 +0000

    Update Stats.py

--- Stats.py
+++ Stats.py
@@ -4,162 +4,154 @@ Created on Wed Nov 30 22:00:22 2022
 
 @author: Huawei
 """
-
 import pandas as pd
 import matplotlib.pyplot as plt
-# import seaborn as sns
-import numpy as np
+
+"""
+Created a function electric to manipulate the data using pandas dataframes which takes a csv file as argument, 
+reads a dataframe in Worldbank format which is electric consumption and returns two dataframes: 
+one with years as columns and one with countries as columns.
+iloc:for selective columns in dataframe
+fillna(0):will replace Nan values with 0
+Transposed(T): is used to return transposed columns
+kind='line': line plot function in pandas
+bbox_to_anchor: to place leegend outside the box
+label:display labels
+savefig:saves image in the directory
+linestyle: we can display different types of line
+"""
 
 def electric(filename):
-    df=pd.read_csv(filename,skiprows=(4))
-    a=df['Country Name']
-    df=df.iloc[15:20,50:55]
-    #print(df['Country Name']=="Belgium")
-    print(df)
-    df = df.fillna(0)
-    print(df)
-    df.insert(loc=0,column='Country Name',value=a)
-    #df.apply(lambda col: pd.Series(col.unique()))
-    #df.insert(loc=0,column='Year',value=b)
-    df=df.dropna(axis=1)
-    df=df.set_index('Country Name').T
-    print(df)
-    # a1=df['Country Name']
-    # b1=df[['2006','2007','2008','2009','2010']]
-    # print(a1)  
-    return df 
-a=electric("electricityconsumption.csv")
-plt.figure(dpi=144)                   #resolution and clarity purpose of graph
-a.plot(kind='line')
-# a.plot(kind='line',a["2006"],a["Azerbaijan"],label="2006",dashes=[1,1])    #calling function to plot line
-# a.plot(kind='line',a["2007"],a["Burundi"],label="2007",dashes=[1,1])  
-# a.plot(kind='line',a["2008"],a["Belgium"],label="2008",dashes=[1,1])  
-# a.plot(kind='line',a["2009"],a["Benin"],label="2009",dashes=[1,1])  
-# a.plot(kind='line',a["2010"],a["Burkina Faso "],label="2010",dashes=[1,1])  
-    #plt.plot(df, df["Female"],"Female","blue")
-plt.xlabel("Countries")
+    df_electric=pd.read_csv(filename,skiprows=(4))
+    df_country=df_electric['Country Name']
+    df_electric=df_electric.iloc[[10,20,40,50,60],[54,55,56,57,58]]
+    df_electric = df_electric.fillna(0)
+    print(df_electric)
+    df_electric.insert(loc=0,column='Country Name',value=df_country)
+    df_electric=df_electric.dropna(axis=1)
+    df_electrict=df_electric.set_index('Country Name').T
+    #print(df)
+    return df_electric,df_electrict 
+a,b=electric("electricityconsumption.csv")
+print(a.describe())
+plt.figure(figsize=(8,10),dpi=150)                   
+b.plot(kind='line',linestyle='-.')
+plt.xlabel("Years")
 plt.ylabel("Energy per Capita")
-plt.title("Electric Consumption per Capita")
-plt.legend(loc="best") 
-
-
-
-def electric(filename):
-    df=pd.read_csv(filename,skiprows=(4))
-    a=df['Country Name']
-    df=df.iloc[15:20,50:55]
-    #print(df['Country Name']=="Belgium")
-    print(df)
-    df = df.fillna(0)
-    print(df)
-    df.insert(loc=0,column='Country Name',value=a)
-    #df.apply(lambda col: pd.Series(col.unique()))
-    #df.insert(loc=0,column='Year',value=b)
-    df=df.dropna(axis=1)
-    df=df.set_index('Country Name').T
-    print(df)
-    # a1=df['Country Name']
-    # b1=df[['2006','2007','2008','2009','2010']]
-    # print(a1)  
-    return df 
-a=electric("co2.csv")
-plt.figure(dpi=144)                   #resolution and clarity purpose of graph
-a.plot(kind='line')
-# a.plot(kind='line',a["2006"],a["Azerbaijan"],label="2006",dashes=[1,1])    #calling function to plot line
-# a.plot(kind='line',a["2007"],a["Burundi"],label="2007",dashes=[1,1])  
-# a.plot(kind='line',a["2008"],a["Belgium"],label="2008",dashes=[1,1])  
-# a.plot(kind='line',a["2009"],a["Benin"],label="2009",dashes=[1,1])  
-# a.plot(kind='line',a["2010"],a["Burkina Faso "],label="2010",dashes=[1,1])  
-    #plt.plot(df, df["Female"],"Female","blue")
-plt.xlabel("Countries")
-plt.ylabel("GDP")
-plt.title("GDP growth")
-plt.legend(loc="best") 
-
-# def co2(filename):
-#      df=pd.read_csv(filename,skiprows=(4))
-#      a=df['Country Name']
-#      df=df.iloc[50:55,50:55]
-#      #print(df['Country Name']=="Belgium")
-#      print(df)
-#      df = df.fillna(0)
-#      print(df)
-#      df.insert(loc=0,column='Country Name',value=a)
-#      #df.apply(lambda col: pd.Series(col.unique()))
-#      #df.insert(loc=0,column='Year',value=b)
-#      df=df.dropna(axis=1)
-#      df1=df.set_index('Country Name').T
-#      print(df)
-#      a1=df['Country Name']
-#      b1=df[['2006','2007','2008','2009','2010']]
-#      print(a1)
-#      #x_axis = np.arange(len(a1))
-#      plt.figure(dpi=144)                   #resolution and clarity purpose of graph
-#      plt.bar(a1,b1["2006"],label="2006")    #calling function to plot line
-#      plt.bar(a1,b1["2007"],bottom=b1["2007"],label="2007")  
-#      plt.bar(a1,b1["2008"],bottom=b1["2008"],label="2008")  
-#      plt.bar(a1,b1["2009"],bottom=b1["2009"],label="2009")  
-#      plt.bar(a1,b1["2010"],bottom=b1["2010"],label="2010")
-#      plt.xlabel("Countries")
-#      plt.ylabel("Energy per Capita")
-#      plt.title("CO2 Emissions")
-#      plt.legend(loc="best")   
-#      return df,df1 
-# a,b=co2("co2.csv")
-def greenhouse(filename):
-     df=pd.read_csv(filename,skiprows=(4))
-     a=df['Country Name']
-     df=df.iloc[20:25,55:60]
-     #print(df['Country Name']=="Belgium")
-     print(df)
-     df = df.fillna(0)
-     print(df)
-     df.insert(loc=0,column='Country Name',value=a)
-     #df.apply(lambda col: pd.Series(col.unique()))
-     #df.insert(loc=0,column='Year',value=b)
-     df=df.dropna(axis=1)
-     df1=df.set_index('Country Name').T
-     print(df)
-     # a1=df['Country Name']
-     # b1=df[['2011','2012','2013','2014','2015']]
-     #print(a1)
-     return df
-a=greenhouse("greenhouse.csv")
-plt.figure(figsize=(10,7),dpi=150) 
-a.plot(kind='bar',x='Country Name')
-plt.xlabel("Countries")
-plt.ylabel("Green house Gas Emissions")
-plt.ylim()
-plt.title("Total greenhouse gas emissions (kt of CO2 equivalent)")
-plt.legend(loc="best") 
-plt.xticks()
+plt.title("Electric power consumption (kWh per capita)")
+plt.legend(loc='center left', bbox_to_anchor=(1, 0.5)) 
+plt.savefig("Electric.png",bbox_inches="tight")
 plt.show()
 
-def forest(filename):
-     df=pd.read_csv(filename,skiprows=(4))
-     a=df['Country Name']
-     df=df.iloc[20:25,55:60]
-     #print(df['Country Name']=="Belgium")
-     print(df)
-     df = df.fillna(0)
-     print(df)
-     df.insert(loc=0,column='Country Name',value=a)
-     #df.apply(lambda col: pd.Series(col.unique()))
-     #df.insert(loc=0,column='Year',value=b)
-     df=df.dropna(axis=1)
-     df1=df.set_index('Country Name').T
-     print(df)
-     # a1=df['Country Name']
-     # b1=df[['2011','2012','2013','2014','2015']]
-     #print(a1)
-     return df
-a=forest("forest.csv")
+"""
+Created a function gdp to manipulate the data using pandas dataframes which takes a csv file as argument, 
+reads a dataframe in Worldbank format which is GDP growth (annual %) and returns two dataframes: 
+one with years as columns and one with countries as columns.
+iloc:for selective columns in dataframe
+fillna(0):will replace Nan values with 0
+Transposed(T): is used to return transposed columns
+kind='line': line plot function in pandas
+bbox_to_anchor: to place leegend outside the box
+label:display labels
+savefig:saves image in the directory
+linestyle: we can display different types of line
+"""
+
+def gdp(filename):
+    df_gdp=pd.read_csv(filename,skiprows=(4))
+    a=df_gdp['Country Name']
+    df_gdp=df_gdp.iloc[[10,20,40,50,60],[54,55,56,57,58]]
+    print(df_gdp)
+    df_gdp = df_gdp.fillna(0)
+    df_gdp.insert(loc=0,column='Country Name',value=a)
+    df_gdp=df_gdp.dropna(axis=1)
+    df_gdpt=df_gdp.set_index('Country Name').T
+    return df_gdp,df_gdpt 
+a,b=gdp("gdp.csv")
+print(a.describe())
+plt.figure(figsize=(8,10),dpi=150)                  
+b.plot(kind='line',linestyle='-.')
+plt.xlabel("Years")
+plt.ylabel("GDP Growth")
+plt.title("GDP growth (annual %)")
+plt.legend(loc='center left', bbox_to_anchor=(1, 0.5)) 
+plt.savefig("GDP.png",bbox_inches="tight")
+plt.show()
+
+"""
+Created a function greenhouse to manipulate the data using pandas dataframes which takes a csv file as argument, 
+reads a dataframe in Worldbank format which is Green house Gas Emissions and returns two dataframes: 
+one with years as columns and one with countries as columns.
+iloc:for selective columns in dataframe
+fillna(0):will replace Nan values with 0
+Transposed(T): is used to return transposed columns
+kind='bar': bar plot function in pandas
+bbox_to_anchor: to place leegend outside the box
+label:display labels
+savefig:saves image in the directory
+linestyle: we can display different types of line
+"""
+
+def greenhouse(filename):
+     df_greenhouse=pd.read_csv(filename,skiprows=(4))
+     a=df_greenhouse['Country Name']
+     df_greenhouse=df_greenhouse.iloc[[55,40,45,70,188],[35,45,55,59,60]]
+     print(df_greenhouse)
+     df_greenhouse = df_greenhouse.fillna(0)
+     print(df_greenhouse)
+     df_greenhouse.insert(loc=0,column='Country Name',value=a)
+     df_greenhouse=df_greenhouse.dropna(axis=1)
+     df_greenhouset=df_greenhouse.set_index('Country Name').T
+     return df_greenhouse,df_greenhouset
+a,b=greenhouse("greenhouse.csv")
+print(a.describe())
 plt.figure(figsize=(10,7),dpi=150) 
 a.plot(kind='bar',x='Country Name')
-plt.xlabel("Countries")
-plt.ylabel("Forest area (% of land area)")
+font = {'family':'serif','color':'darkred','size':10}
+plt.xlabel("Countries",fontdict=(font))
+plt.ylabel("Green house Gas Emissions",fontdict=(font))
 plt.ylim()
+plt.xticks(rotation=45)
+plt.title("Total greenhouse gas emissions")
+plt.legend(loc='center left', bbox_to_anchor=(1, 0.5)) 
+plt.savefig("greenhouse.png",bbox_inches="tight")
+plt.show()
+
+"""
+Created a function forest to manipulate the data using pandas dataframes which takes a csv file as argument, 
+reads a dataframe in Worldbank format which is Forest area (% of land area) and returns two dataframes: 
+one with years as columns and one with countries as columns.
+iloc:for selective columns in dataframe
+fillna(0):will replace Nan values with 0
+Transposed(T): is used to return transposed columns
+kind='bar': bar plot function in pandas
+bbox_to_anchor: to place leegend outside the box
+label:display labels
+savefig:saves image in the directory
+linestyle: we can display different types of line
+"""
+def forest(filename):
+     df_forest=pd.read_csv(filename,skiprows=(4))
+     a=df_forest['Country Name']
+     df_forest=df_forest.iloc[[55,40,45,70,188],[35,45,55,59,60]]
+     print(df_forest)
+     df_forest = df_forest.fillna(0)
+     print(df_forest)
+     df_forest.insert(loc=0,column='Country Name',value=a)
+     df_forest=df_forest.dropna(axis=1)
+     df_forestt=df_forest.set_index('Country Name').T
+     print(df_forest)
+     return df_forest,df_forestt
+a,b=forest("forest.csv")
+print(a.describe())
+plt.figure(figsize=(10,7),dpi=150) 
+a.plot(kind='bar',x='Country Name')
+font = {'family':'serif','color':'darkred','size':10}
+plt.xlabel("Countries",fontdict=(font))
+plt.ylabel("Forest area (% of land area)",fontdict=(font))
+plt.ylim()
+plt.xticks(rotation=45)
 plt.title("Forest Area")
-plt.legend(loc="best") 
-plt.xticks()
+plt.legend(loc='center left', bbox_to_anchor=(1, 0.5)) 
+plt.savefig("forest.png",bbox_inches="tight")
 plt.show()
